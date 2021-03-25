5e05246e255f2217734e9470240c9c62004e61f8
MykhailoSiomachok <62886029+MykhailoSiomachok@users.noreply.github.com>
2021-03-25 09:35:09 +0200

    Revert "Revert "View bug fix""
    This reverts commit 8222ca749e82a371242d73c35c999b0836b1ea17.

--- ScheduleWebApp/ScheduleWebApp/Controllers/ScheduleTableController.cs
+++ ScheduleWebApp/ScheduleWebApp/Controllers/ScheduleTableController.cs
@@ -12,16 +12,22 @@ namespace ScheduleWebApp.Controllers
     {
         private readonly ILogger<ScheduleTableController> _logger;
         private readonly dfkg9ojh16b4rdContext _context;
-
+        private ScheduleTable scheduleTable;
         public ScheduleTableController(ILogger<ScheduleTableController> logger, dfkg9ojh16b4rdContext context)
         {
             _logger = logger;
             _context = context;
+            scheduleTable = new ScheduleTable(_context);
         }
         public List<Faculty> Show()
         {
             ScheduleTable scheduleTable = new ScheduleTable(_context);
             return scheduleTable.Faculties;
+        }
+        public List<FacultyGroup> GetFacultyGroups()
+        {
+            scheduleTable.SetFacultyName("aaaa");
+            return scheduleTable.Groups;
         }
         public IActionResult Table()
         {
